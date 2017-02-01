86c81c67a2b5fbe00cb03861b3ebbca4c6a8e6c3
encorechow <encorechow1992@gmail.com>
2017-01-31 21:53:27 -0800

    a couple of more codes

---
+++ rnn/rnn_example.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.ops import rnn, rnn_cell
+'''
+input > weighted >hidden layer 1 (activation function) > weights
+> hidden layer 2 (activation function) > weights > output layer
+
+compare output to intended output > cost / loss function (cross entropy)
+optimization function (optimizer) > minimize cost (AdamOptimizer...SGD, AdaGrad)
+
+backpropagation
+
+
+feed forward + backprop = epoch
+
+'''
+
+'''
+10 classes 0-9 numbers
+one_hot examples:
+    0 = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+'''
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+epochs = 10
+n_classes = 10
+batch_size = 128
+
+
+
+chunk_size = 28
+n_chunks = 28
+rnn_size = 128
+
+x = tf.placeholder('float', [None,n_chunks, chunk_size])
+y = tf.placeholder('float', [None, 10])
+
+def recurrent_neural_network(x):
+
+    # (input_data * weights) + biases
+
+    layer = {
+                'weights': tf.Variable(tf.random_normal([rnn_size, n_classes])),
+                'biases': tf.Variable(tf.random_normal([n_classes]))
+            }
+
+    x = tf.transpose(x, [1, 0, 2])
+    x = tf.reshape(x, [-1, chunk_size])
+    x = tf.split(0, n_chunks, x)
+
+    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
+    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+
+    # output
+    output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
+
+    return output
+
+
+def train_neural_network(x):
+
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+
+    # default learning rate = 0.001 for AdamOptimi zer
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+
+        for epoch in range(epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+                batch_x = batch_x.reshape((batch_size, n_chunks, chunk_size))
+
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of ', epochs, ' loss:', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        print('Accuracy:', accuracy.eval({x: mnist.test.images.reshape((-1, n_chunks, chunk_size)), y: mnist.test.labels}))
+
+
+train_neural_network(x)
